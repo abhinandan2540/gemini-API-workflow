849cc821224f1c7724d844aad0d9c69362739eeb
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2024-12-21 22:31:22 +0530

    saving models Room,Topic,Message into admin.py

--- geminiwf/base/admin.py
+++ geminiwf/base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import TextModelHistory, ImageModelHistory, interactiveChatHistory, QubeVisionImgHistory, QubeVideoModelHistory
 from .models import QubeAudioModelHistory, QubeCodeModelHistory
-from base.models import Room
+from base.models import Room, Topic, Message
 
 
 admin.site.register(TextModelHistory),
@@ -12,3 +12,5 @@ admin.site.register(QubeVideoModelHistory)
 admin.site.register(QubeAudioModelHistory)
 admin.site.register(QubeCodeModelHistory)
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
